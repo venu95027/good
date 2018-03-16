88eaedf731695af1c45be7aacfe8574baf3fa2c5
nagoor9154 <34703233+nagoor9154@users.noreply.github.com>
2018-03-16 12:29:13 +0530

    Update App.java

--- src/main/java/com/gamutkart/App.java
+++ src/main/java/com/gamutkart/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello welcome to Gamut!" );
+        System.out.println( "Hello welcome to GamutGurus!" );
     }
 }
